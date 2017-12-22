d9a70e328df0e511b88ff84208c93e95fa2a14ae
lichenabson <1114795127@qq.com>
2017-12-22 17:21:34 +0800

    add lot of function

--- src/router/index.js
+++ src/router/index.js
@@ -29,3 +29,99 @@ export default new Router({
     }
   ]
 })
+
+/*var array=[1,2,1,8,79,8],max=null;
+for(var i in array){
+  if(array.hasOwnProperty(i)){
+    max=Math.max(max,array[i])
+  }
+}
+
+console.log(max);*/
+
+/*var array=[1,2,1,8,79,8],functionArray=[];
+for(var i in array){
+  if(array.hasOwnProperty(i)){
+    functionArray.push(
+      function(){
+        alert(array[i])
+      }
+    )
+  }
+}*/
+
+/*var obj=[
+  {
+    a:1,b:2,c:3
+  },{
+    a:1,b:{
+      a:1,b:2,c:3
+    }
+  },
+  {
+    a:1,b:2,c:3,d:1
+  },{
+    a:1,b:2,c:3,d:1,g:6
+  }
+];
+
+obj.sort(function(a,b){
+  return  Object.getOwnPropertyNames(a).length-Object.getOwnPropertyNames(b).length
+});*/
+
+/*function a(){
+  var a=1,b=1,c;
+  console.log(a);
+  console.log(b);
+  return (function (){
+    for(var i=0;i<100;i++){
+      c=a+b;
+      a=b;
+      b=c;
+      console.log(c);
+    }
+  }())
+}*/
+
+
+/*
+Number.prototype.plus=function(a){
+  if(typeof a==="number")return this+a
+};
+
+Number.prototype.minus=function(a){
+  if(typeof a==="number") return this-a
+};
+*/
+
+//柯里化函数
+/*function add(a){
+  var s;
+    return function (b){
+      return b+a
+    }
+}*/
+
+
+function add(x) {
+  var result=function(y){
+    add(x*y);
+  };
+  result.valueOf=function(){
+    return x
+  };
+  return result;
+}
+
+function mul(x) {
+  const result =function(y){
+    return mul(x * y)
+  };
+  result.valueOf =function(){
+    return x
+  };
+  return result;
+}
+
+
+
