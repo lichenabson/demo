e041d154c3a4f0c80bc07acabc335e58710d0460
lichenabson <1114795127@qq.com>
2018-02-07 16:26:22 +0800

    add lib of fastclick to resolve some problem

--- msui/index.js
+++ msui/index.js
@@ -1,18 +1,25 @@
 $(function(){
   //严格模式
   'use strict';
+
+  if ('addEventListener' in document) {
+    document.addEventListener('DOMContentLoaded', function() {
+      FastClick.attach(document.body);
+    }, false);
+  }
+
   $(document).on("pageInit", "#index", function() {
-    $("#attendance-class-list").on("tap", "li.item-content", function() {
+    $("#attendance-class-list").on("click", "li.item-content", function() {
       $.router.load("list.html", false);
     });
   });
   $(document).on("pageInit", "#list", function() {
-    $("#attendance-student-list").on("tap", "li", function() {
+    $("#attendance-student-list").on("click", "li", function() {
       $.router.load("detail.html", false);
     });
   });
   $(document).on("pageInit", "#index-class", function() {
-    $("#attendance-student-list-class").on("tap", "li", function() {
+    $("#attendance-student-list-class").on("click", "li", function() {
       $.router.load("detail.html", false);
     });
 
